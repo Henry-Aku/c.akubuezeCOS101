943f96e3de5cb7616e19208af86f758a12beda72
Henry Akubueze <zayadhenry@gmail.com>
2025-10-15 16:45:36 +0100

    Week-3 practice

--- Week-3/practice_10/src/main.rs
+++ Week-3/practice_10/src/main.rs
@@ -1,14 +1,14 @@
 fn main() {
     //addition
     let sum = 5550 + 7310;
-    println!("The sum of 5550 and 7310 - {}", sum);
+    println!("The sum of 5550 and 7310 is {}", sum);
 
     let difference:f64 = 95.5 - 4.3;
-    println!("The difference of 95.5 and 4.3 - {}", difference);
+    println!("The difference of 95.5 and 4.3 is {}", difference);
 
     let product:i32 = 4 * 30;
-    println!("The division of 56.7 and 32.2 {}", product);
+    println!("The product of 56.7 and 32.2 is {}", product);
 
-    let remainder = 43 % 5;
+    let remainder:i32 = 43 % 5;
     println!("The remainder of 43 and 5 {}", remainder);
 }
