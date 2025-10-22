91bd13fb6122d5d5cfecd5b662977512c0451046
Henry Akubueze <zayadhenry@gmail.com>
2025-10-22 13:25:09 +0100

    Practice 2

---
+++ week-4/practice_2/target/debug/deps/practice_2.d
@@ -0,0 +1,5 @@
+C:\Users\zayad\OneDrive\Documents\c.akubuezeCOS101\week-4\practice_2\target\debug\deps\practice_2.d: src\main.rs
+
+C:\Users\zayad\OneDrive\Documents\c.akubuezeCOS101\week-4\practice_2\target\debug\deps\practice_2.exe: src\main.rs
+
+src\main.rs:
