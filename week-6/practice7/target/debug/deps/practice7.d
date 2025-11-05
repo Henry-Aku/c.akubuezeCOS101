a6cb396c9f869210b97fc91111ec006b9c5037dc
Henry Akubueze <zayadhenry@gmail.com>
2025-11-05 21:42:55 +0100

    PrCTICE7

---
+++ week-6/practice7/target/debug/deps/practice7.d
@@ -0,0 +1,5 @@
+C:\Users\zayad\OneDrive\Documents\c.akubuezeCOS101\week-6\practice7\target\debug\deps\practice7.d: src\main.rs
+
+C:\Users\zayad\OneDrive\Documents\c.akubuezeCOS101\week-6\practice7\target\debug\deps\practice7.exe: src\main.rs
+
+src\main.rs:
