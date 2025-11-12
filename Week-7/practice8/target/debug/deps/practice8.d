f2f102b494abf8df6be3514554261dfbc87fb3f1
Henry Akubueze <zayadhenry@gmail.com>
2025-11-12 19:37:27 +0100

    Practice 8

---
+++ Week-7/practice8/target/debug/deps/practice8.d
@@ -0,0 +1,5 @@
+C:\Users\zayad\OneDrive\Documents\c.akubuezeCOS101\Week-7\practice8\target\debug\deps\practice8.d: src\main.rs
+
+C:\Users\zayad\OneDrive\Documents\c.akubuezeCOS101\Week-7\practice8\target\debug\deps\practice8.exe: src\main.rs
+
+src\main.rs:
---
+++ Week-7/practice8/target/debug/practice8.d
@@ -0,0 +1,1 @@
+C:\Users\zayad\OneDrive\Documents\c.akubuezeCOS101\Week-7\practice8\target\debug\practice8.exe: C:\Users\zayad\OneDrive\Documents\c.akubuezeCOS101\Week-7\practice8\src\main.rs
