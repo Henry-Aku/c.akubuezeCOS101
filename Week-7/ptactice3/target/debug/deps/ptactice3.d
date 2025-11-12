6a06d8ec6782eba5dcbf306f7389d5889dfc8f22
Henry Akubueze <zayadhenry@gmail.com>
2025-11-12 13:46:35 +0100

    practice3

---
+++ Week-7/ptactice3/target/debug/deps/ptactice3.d
@@ -0,0 +1,5 @@
+C:\Users\zayad\OneDrive\Documents\c.akubuezeCOS101\Week-7\ptactice3\target\debug\deps\ptactice3.d: src\main.rs
+
+C:\Users\zayad\OneDrive\Documents\c.akubuezeCOS101\Week-7\ptactice3\target\debug\deps\ptactice3.exe: src\main.rs
+
+src\main.rs:
---
+++ Week-7/ptactice3/target/debug/ptactice3.d
@@ -0,0 +1,1 @@
+C:\Users\zayad\OneDrive\Documents\c.akubuezeCOS101\Week-7\ptactice3\target\debug\ptactice3.exe: C:\Users\zayad\OneDrive\Documents\c.akubuezeCOS101\Week-7\ptactice3\src\main.rs
