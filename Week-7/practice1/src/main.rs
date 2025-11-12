450123fd9dd9441b9c82557fa34054edbe2b2ff9
Henry Akubueze <zayadhenry@gmail.com>
2025-11-12 13:33:38 +0100

    practice 1

---
+++ Week-7/practice1/src/main.rs
@@ -0,0 +1,8 @@
+fn my_grade(){
+
+    println!("Greeting Citizen from the outer world");
+}
+
+fn main() {
+    my_grade();
+}
---
+++ Week-7/practice1/target/debug/practice1.d
@@ -0,0 +1,1 @@
+C:\Users\zayad\OneDrive\Documents\c.akubuezeCOS101\Week-7\practice1\target\debug\practice1.exe: C:\Users\zayad\OneDrive\Documents\c.akubuezeCOS101\Week-7\practice1\src\main.rs
