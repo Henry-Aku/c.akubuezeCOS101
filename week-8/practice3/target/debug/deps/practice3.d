12e36f9454562e0c0493def70cd1364c678fea12
Henry Akubueze <zayadhenry@gmail.com>
2025-11-19 13:18:16 +0100

    Practice3 get method

---
+++ week-8/practice3/target/debug/deps/practice3.d
@@ -0,0 +1,5 @@
+C:\Users\zayad\OneDrive\Documents\c.akubuezeCOS101\week-8\practice3\target\debug\deps\practice3.d: src\main.rs
+
+C:\Users\zayad\OneDrive\Documents\c.akubuezeCOS101\week-8\practice3\target\debug\deps\practice3.exe: src\main.rs
+
+src\main.rs:
