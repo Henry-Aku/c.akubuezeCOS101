04f9e8e77ec8d85c1799ec0a52ff7fe726ea945b
Henry Akubueze <zayadhenry@gmail.com>
2025-11-19 23:01:24 +0100

    Practice11 Rust Slice

---
+++ week-8/practice11/target/debug/deps/practice11.d
@@ -0,0 +1,5 @@
+C:\Users\zayad\OneDrive\Documents\c.akubuezeCOS101\week-8\practice11\target\debug\deps\practice11.d: src\main.rs
+
+C:\Users\zayad\OneDrive\Documents\c.akubuezeCOS101\week-8\practice11\target\debug\deps\practice11.exe: src\main.rs
+
+src\main.rs:
---
+++ week-8/practice11/target/debug/practice11.d
@@ -0,0 +1,1 @@
+C:\Users\zayad\OneDrive\Documents\c.akubuezeCOS101\week-8\practice11\target\debug\practice11.exe: C:\Users\zayad\OneDrive\Documents\c.akubuezeCOS101\week-8\practice11\src\main.rs
