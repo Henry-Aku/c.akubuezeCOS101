e5956465d9a99f4476ceeadcda15f31839116c37
Henry Akubueze <zayadhenry@gmail.com>
2025-11-19 22:32:12 +0100

    Practice8 Mutable Tuples

---
+++ week-8/practice8/target/debug/deps/practice8.d
@@ -0,0 +1,5 @@
+C:\Users\zayad\OneDrive\Documents\c.akubuezeCOS101\week-8\practice8\target\debug\deps\practice8.d: src\main.rs
+
+C:\Users\zayad\OneDrive\Documents\c.akubuezeCOS101\week-8\practice8\target\debug\deps\practice8.exe: src\main.rs
+
+src\main.rs:
---
+++ week-8/practice8/target/debug/practice8.d
@@ -0,0 +1,1 @@
+C:\Users\zayad\OneDrive\Documents\c.akubuezeCOS101\week-8\practice8\target\debug\practice8.exe: C:\Users\zayad\OneDrive\Documents\c.akubuezeCOS101\week-8\practice8\src\main.rs
