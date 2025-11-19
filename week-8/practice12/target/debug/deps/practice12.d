b35a84c8ac86936e1bf04343bab48227efb3e3d0
Henry Akubueze <zayadhenry@gmail.com>
2025-11-19 23:09:34 +0100

    Practice12 Mutable Slices

---
+++ week-8/practice12/target/debug/deps/practice12.d
@@ -0,0 +1,5 @@
+C:\Users\zayad\OneDrive\Documents\c.akubuezeCOS101\week-8\practice12\target\debug\deps\practice12.d: src\main.rs
+
+C:\Users\zayad\OneDrive\Documents\c.akubuezeCOS101\week-8\practice12\target\debug\deps\practice12.exe: src\main.rs
+
+src\main.rs:
---
+++ week-8/practice12/target/debug/practice12.d
@@ -0,0 +1,1 @@
+C:\Users\zayad\OneDrive\Documents\c.akubuezeCOS101\week-8\practice12\target\debug\practice12.exe: C:\Users\zayad\OneDrive\Documents\c.akubuezeCOS101\week-8\practice12\src\main.rs
