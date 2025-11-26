a8577d5053e213ca002b386efa98b41661c1b999
Henry Akubueze <zayadhenry@gmail.com>
2025-11-26 13:06:18 +0100

    Appending Data to a rust file

---
+++ week-9/practice4/target/debug/deps/practice4.d
@@ -0,0 +1,5 @@
+C:\Users\zayad\OneDrive\Documents\c.akubuezeCOS101\week-9\practice4\target\debug\deps\practice4.d: src\main.rs
+
+C:\Users\zayad\OneDrive\Documents\c.akubuezeCOS101\week-9\practice4\target\debug\deps\practice4.exe: src\main.rs
+
+src\main.rs:
