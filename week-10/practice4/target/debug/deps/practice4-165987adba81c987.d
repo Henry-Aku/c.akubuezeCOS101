87514d1b5efe238b54d0b349b0985dba1f1cbda2
Henry Akubueze <zayadhenry@gmail.com>
2025-12-03 13:24:44 +0100

    Transferring ownership through arguments

---
+++ week-10/practice4/target/debug/deps/practice4-165987adba81c987.d
@@ -0,0 +1,5 @@
+C:\Users\zayad\OneDrive\Documents\c.akubuezeCOS101\week-10\practice4\target\debug\deps\practice4-165987adba81c987.d: src\main.rs
+
+C:\Users\zayad\OneDrive\Documents\c.akubuezeCOS101\week-10\practice4\target\debug\deps\libpractice4-165987adba81c987.rmeta: src\main.rs
+
+src\main.rs:
---
+++ week-10/practice4/target/debug/deps/practice4-aac492077cf4d0f1.d
@@ -0,0 +1,5 @@
+C:\Users\zayad\OneDrive\Documents\c.akubuezeCOS101\week-10\practice4\target\debug\deps\practice4-aac492077cf4d0f1.d: src\main.rs
+
+C:\Users\zayad\OneDrive\Documents\c.akubuezeCOS101\week-10\practice4\target\debug\deps\libpractice4-aac492077cf4d0f1.rmeta: src\main.rs
+
+src\main.rs:
