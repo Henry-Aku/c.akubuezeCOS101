333c4caa98232cad52d57e0ca5d77c11b44e8847
Henry Akubueze <zayadhenry@gmail.com>
2025-12-03 13:10:13 +0100

    Assigning value from one variable to another

---
+++ week-10/practice1/target/debug/deps/practice1-b3b73d479043b585.d
@@ -0,0 +1,5 @@
+C:\Users\zayad\OneDrive\Documents\c.akubuezeCOS101\week-10\practice1\target\debug\deps\practice1-b3b73d479043b585.d: src\main.rs
+
+C:\Users\zayad\OneDrive\Documents\c.akubuezeCOS101\week-10\practice1\target\debug\deps\libpractice1-b3b73d479043b585.rmeta: src\main.rs
+
+src\main.rs:
---
+++ week-10/practice1/target/debug/deps/practice1-de2dc7c835f619a7.d
@@ -0,0 +1,5 @@
+C:\Users\zayad\OneDrive\Documents\c.akubuezeCOS101\week-10\practice1\target\debug\deps\practice1-de2dc7c835f619a7.d: src\main.rs
+
+C:\Users\zayad\OneDrive\Documents\c.akubuezeCOS101\week-10\practice1\target\debug\deps\libpractice1-de2dc7c835f619a7.rmeta: src\main.rs
+
+src\main.rs:
