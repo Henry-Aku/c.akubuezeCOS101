a0ce8a54e71148e8f74b79122ba898c2a6e1cc52
Henry Akubueze <zayadhenry@gmail.com>
2025-12-03 13:16:10 +0100

    Passing value to a function

---
+++ week-10/practice2/target/debug/deps/practice2-0a18e97068e67f7e.d
@@ -0,0 +1,5 @@
+C:\Users\zayad\OneDrive\Documents\c.akubuezeCOS101\week-10\practice2\target\debug\deps\practice2-0a18e97068e67f7e.d: src\main.rs
+
+C:\Users\zayad\OneDrive\Documents\c.akubuezeCOS101\week-10\practice2\target\debug\deps\libpractice2-0a18e97068e67f7e.rmeta: src\main.rs
+
+src\main.rs:
---
+++ week-10/practice2/target/debug/deps/practice2-bc41bdd55855fb23.d
@@ -0,0 +1,5 @@
+C:\Users\zayad\OneDrive\Documents\c.akubuezeCOS101\week-10\practice2\target\debug\deps\practice2-bc41bdd55855fb23.d: src\main.rs
+
+C:\Users\zayad\OneDrive\Documents\c.akubuezeCOS101\week-10\practice2\target\debug\deps\libpractice2-bc41bdd55855fb23.rmeta: src\main.rs
+
+src\main.rs:
