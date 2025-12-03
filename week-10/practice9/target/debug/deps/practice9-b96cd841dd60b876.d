d2f408b9370ea191e25b133bad4c61ff0db9777c
Henry Akubueze <zayadhenry@gmail.com>
2025-12-03 13:47:56 +0100

    Implementing methods with structure

---
+++ week-10/practice9/target/debug/deps/practice9-b96cd841dd60b876.d
@@ -0,0 +1,5 @@
+C:\Users\zayad\OneDrive\Documents\c.akubuezeCOS101\week-10\practice9\target\debug\deps\practice9-b96cd841dd60b876.d: src\main.rs
+
+C:\Users\zayad\OneDrive\Documents\c.akubuezeCOS101\week-10\practice9\target\debug\deps\libpractice9-b96cd841dd60b876.rmeta: src\main.rs
+
+src\main.rs:
---
+++ week-10/practice9/target/debug/deps/practice9-e14b2359408d0a54.d
@@ -0,0 +1,5 @@
+C:\Users\zayad\OneDrive\Documents\c.akubuezeCOS101\week-10\practice9\target\debug\deps\practice9-e14b2359408d0a54.d: src\main.rs
+
+C:\Users\zayad\OneDrive\Documents\c.akubuezeCOS101\week-10\practice9\target\debug\deps\libpractice9-e14b2359408d0a54.rmeta: src\main.rs
+
+src\main.rs:
