d9aafd35617e5f9c724e2de0e2810f09f61c22db
Henry Akubueze <zayadhenry@gmail.com>
2025-12-03 13:19:59 +0100

    Returning value from a function

---
+++ week-10/practice3/target/debug/deps/practice3-4242c82789c4ec87.d
@@ -0,0 +1,5 @@
+C:\Users\zayad\OneDrive\Documents\c.akubuezeCOS101\week-10\practice3\target\debug\deps\practice3-4242c82789c4ec87.d: src\main.rs
+
+C:\Users\zayad\OneDrive\Documents\c.akubuezeCOS101\week-10\practice3\target\debug\deps\libpractice3-4242c82789c4ec87.rmeta: src\main.rs
+
+src\main.rs:
---
+++ week-10/practice3/target/debug/deps/practice3-71f1c68bc890a370.d
@@ -0,0 +1,5 @@
+C:\Users\zayad\OneDrive\Documents\c.akubuezeCOS101\week-10\practice3\target\debug\deps\practice3-71f1c68bc890a370.d: src\main.rs
+
+C:\Users\zayad\OneDrive\Documents\c.akubuezeCOS101\week-10\practice3\target\debug\deps\libpractice3-71f1c68bc890a370.rmeta: src\main.rs
+
+src\main.rs:
