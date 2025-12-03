9ed79152df8f48638331b3f4066ec353b7e35d80
Henry Akubueze <zayadhenry@gmail.com>
2025-12-03 13:37:17 +0100

    Implementing Structs in rust

---
+++ week-10/practice7/target/debug/deps/practice7-69cef74ac8a0b7e3.d
@@ -0,0 +1,5 @@
+C:\Users\zayad\OneDrive\Documents\c.akubuezeCOS101\week-10\practice7\target\debug\deps\practice7-69cef74ac8a0b7e3.d: src\main.rs
+
+C:\Users\zayad\OneDrive\Documents\c.akubuezeCOS101\week-10\practice7\target\debug\deps\libpractice7-69cef74ac8a0b7e3.rmeta: src\main.rs
+
+src\main.rs:
---
+++ week-10/practice7/target/debug/deps/practice7-e4815d128bab0cf4.d
@@ -0,0 +1,5 @@
+C:\Users\zayad\OneDrive\Documents\c.akubuezeCOS101\week-10\practice7\target\debug\deps\practice7-e4815d128bab0cf4.d: src\main.rs
+
+C:\Users\zayad\OneDrive\Documents\c.akubuezeCOS101\week-10\practice7\target\debug\deps\libpractice7-e4815d128bab0cf4.rmeta: src\main.rs
+
+src\main.rs:
