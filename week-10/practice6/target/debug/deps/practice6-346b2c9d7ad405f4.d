be6bfbbe606d05e9b8101be3c250bea6991f2a50
Henry Akubueze <zayadhenry@gmail.com>
2025-12-03 13:32:13 +0100

    Applying mutable references

---
+++ week-10/practice6/target/debug/deps/practice6-346b2c9d7ad405f4.d
@@ -0,0 +1,5 @@
+C:\Users\zayad\OneDrive\Documents\c.akubuezeCOS101\week-10\practice6\target\debug\deps\practice6-346b2c9d7ad405f4.d: src\main.rs
+
+C:\Users\zayad\OneDrive\Documents\c.akubuezeCOS101\week-10\practice6\target\debug\deps\libpractice6-346b2c9d7ad405f4.rmeta: src\main.rs
+
+src\main.rs:
---
+++ week-10/practice6/target/debug/deps/practice6-58d32ca2c5274661.d
@@ -0,0 +1,5 @@
+C:\Users\zayad\OneDrive\Documents\c.akubuezeCOS101\week-10\practice6\target\debug\deps\practice6-58d32ca2c5274661.d: src\main.rs
+
+C:\Users\zayad\OneDrive\Documents\c.akubuezeCOS101\week-10\practice6\target\debug\deps\libpractice6-58d32ca2c5274661.rmeta: src\main.rs
+
+src\main.rs:
